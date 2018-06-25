0f4753872f2055ee1a4ae017a1d9f545bb8b39cd
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2018-06-25 11:24:34 +0900

    add solution to support for uwp

--- RosBridgeClient/Package4UnityEditor/Properties/AssemblyInfo.cs
+++ RosBridgeClient/Package4UnityEditor/Properties/AssemblyInfo.cs
@@ -1,34 +1,16 @@
-﻿/*
-© Siemens AG, 2017
-Author: Dr. Martin Bischoff (martin.bischoff@siemens.com)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-<http://www.apache.org/licenses/LICENSE-2.0>.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RosBridgeClient")]
+[assembly: AssemblyTitle("Package4UnityEditor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Licence Owner")]
-[assembly: AssemblyProduct("RosBridgeClient")]
-[assembly: AssemblyCopyright("Copyright © Licence Owner 2016")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Package4UnityEditor")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -38,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("27cd898a-8840-4cdd-a475-4a672fd2ef50")]
+[assembly: Guid("a73284bd-d551-43fd-b9ce-8858e45ecfef")]
 
 // Version information for an assembly consists of the following four values:
 //
